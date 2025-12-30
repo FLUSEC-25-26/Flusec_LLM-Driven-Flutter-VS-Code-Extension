9cb00f65d7fb9e7d9a0d8bb7f5e4a48e9ce9c436
Hasintha Madushan <hasinthagunawardana2000@gmail.com>
2025-12-30 20:54:41 +0530

    feat: update index dart file with adding ids file parths

--- flusec/dart-analyzer/lib/ids/index.dart
+++ flusec/dart-analyzer/lib/ids/index.dart
@@ -1,7 +1,12 @@
 // lib/ids/index.dart
 //
-// Convenience barrel file for the IDS (Insecure Data Storage) component.
+// Convenience barrel// lib/ids/index.dart
+//
+// IDS (Insecure Data Storage) module exports
+// Single import point for all IDS functionality
 
+export 'ids_engine.dart';
+export 'ids_models.dart';
 export 'insecure_storage_rules.dart';
 export 'storage_visitor.dart';
 export 'heuristic_analyzer.dart';
